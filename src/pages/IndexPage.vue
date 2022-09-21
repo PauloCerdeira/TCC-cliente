9afc941bd32c50d6686859936f71cf7baa16d8c4
Paulo <paulocerdeira1999@gmail.com>
2022-09-21 10:22:23 -0300

    ajustes

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -21,25 +21,54 @@
               <span v-if="result.state">{{ result.state }}, </span>
               {{ result.country }}
             </div>
-            <div class="col"></div>
             lat: {{ result.lat.toFixed(3) }} <br />
             lon: {{ result.lon.toFixed(3) }}
             <br />
           </q-item>
         </q-list>
-        <div v-else>CARREGANDO PORRAAAAAAAAAAAA</div>
+        <div v-else-if="!search_pending" class="q-pa-md fit flex">
+          <h6 class="q-ma-none">Nenhum resultado encontrado.</h6>
+        </div>
+        <div class="q-pa-md fit flex" v-else>
+          <q-spinner class="q-mx-auto" color="primary" size="3em" />
+        </div>
       </q-menu>
     </q-input>
 
-    <div v-if="currentWheater" class="q-mx-auto rpw">
-      <div class="q-mb-sm q-mt-md col-12">
-        <span v-if="currentWheater.state">{{ currentWheater.state }}, </span>
-        {{ currentWheater.sys.country }}
+    <div v-if="selectedLocation" class="q-mx-auto row">
+      <div class="col-6 text-h5 q-py-md q-px-sm">
+        {{ Math.ceil(selectedLocation.main.temp) }} °C
+        <div class="text-caption">
+          Sensação térmica: {{ Math.ceil(selectedLocation.main.feels_like) }} °C
+          <br />
+          <q-icon color="red" name="arrow_upward" />
+          {{ Math.ceil(selectedLocation.main.temp_max) }} °C
+          <q-icon color="blue" name="arrow_downward" />
+          {{ Math.ceil(selectedLocation.main.temp_min) }} °C
+        </div>
       </div>
-
-      <h4 class="q-mt-none">
-        {{ currentWheater.main.temp }} °C - {{ currentWheater.name }}
-      </h4>
+      <div class="col-6 row text-h5 q-py-md q-px-sm items-center">
+        <div class="col-12 row justify-center">
+          {{ selectedLocation.name }}
+        </div>
+        <div class="text-subtitle1 col-12 row justify-center">
+          <span class="q-pr-xs" v-if="selectedLocation.state"
+            >{{ selectedLocation.state }},
+          </span>
+          {{ selectedLocation.sys.country }}
+        </div>
+      </div>
+      <div class="col-12">
+        <span v-for="weather in selectedLocation.weather" :key="weather.id"
+          >{{ weather.description }} <br
+        /></span>
+      </div>
+      <div class="col-12">
+        Umidade: {{ selectedLocation.main.humidity }} % <br />
+        nuvens: {{ selectedLocation.clouds.all }} % <br />
+        vel vento: {{ selectedLocation.wind.speed * 3.6 }} km/h <br />
+        deg vento: {{ selectedLocation.wind.deg }} <br />
+      </div>
     </div>
   </q-page>
 </template>
@@ -51,27 +80,27 @@ export default defineComponent({
   name: "IndexPage",
   data() {
     return {
-      search_location: "",
+      search_location: "Carrão",
       search_cooldown: null,
       search_results: [],
       search_pending: false,
+      last_search: "",
       showAutoComplete: false,
       openWeatherKey: "27d6d870bd455bfeda31b8f1eb2118e1",
-      currentWheater: null,
+      selectedLocation: null,
     };
   },
   async created() {
-    this.currentWheater = (
-      await this.$openWeatherApi.get(
-        `/data/2.5/weather?id=524901&appid=${this.openWeatherKey}&units=metric&lang=pt_br`
-      )
-    ).data;
-    console.log(this.currentWheater);
+    await this.getLocations();
+    await this.getWeather(this.search_results[0]);
+    this.showAutoComplete = false;
   },
   methods: {
     async getLocations() {
       console.log("rodou", this.search_location);
       if (this.search_location.trim().length > 2) {
+        if (this.search_location.trim() == this.last_search.trim()) return;
+        this.last_search = this.search_location;
         this.showAutoComplete = true;
         let now = Date.now();
         let elapsedTime = now - this.search_cooldown;
@@ -89,6 +118,7 @@ export default defineComponent({
           this.search_pending = true;
           setTimeout(async () => {
             if (this.search_location.trim().length > 2) {
+              this.last_search = this.search_location;
               this.search_results = (
                 await this.$openWeatherApi.get(
                   `/geo/1.0/direct?q=${this.search_location.trim()}&limit=5&appid=${
@@ -106,24 +136,24 @@ export default defineComponent({
       } else {
         this.showAutoComplete = false;
       }
-      //   this.currentWheater = (
+      //   this.selectedLocation = (
       //   await this.$openWeatherApi.get(
       //     `/data/2.5/weather?id=524901&appid=${this.openWeatherKey}&units=metric&lang=pt_br`
       //   )
       // ).data;
-      // console.log(this.currentWheater);
+      // console.log(this.selectedLocation);
     },
     async getWeather(location) {
-      this.currentWheater = (
+      this.selectedLocation = (
         await this.$openWeatherApi.get(
           `/data/2.5/weather?lat=${location.lat}&lon=${location.lon}&appid=${this.openWeatherKey}&units=metric&lang=pt_br`
         )
       ).data;
-      this.currentWheater.name = location.name;
-      this.currentWheater.state = location.state;
+      this.selectedLocation.name = location.name;
+      this.selectedLocation.state = location.state;
       this.search_location = location.name;
       this.search_results = [];
-      console.log(this.currentWheater);
+      console.log(this.selectedLocation);
     },
   },
 });
